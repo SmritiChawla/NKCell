0d0be8dd0508cb86da459747d17323cfd97a0208
Smriti Chawla <chawlasmriti_1990@yahoo.co.in>
2022-07-05 15:40:59 +0530

    Update Module1.R

--- Figure2/Fig2d/Module1.R
+++ Figure2/Fig2d/Module1.R
@@ -3,11 +3,11 @@ library(igraph)
 library(networkD3)
 
 ###load Module 1 data
-mod2 = read.table("Module1.csv",sep=",",header=T,stringsAsFactors = F)
+mod1 = read.table("Module1.csv",sep=",",header=T,stringsAsFactors = F)
 
 ##Plotting graph
-p <- simpleNetwork(mod2)
-g <- graph.data.frame(d = mod2, directed = FALSE)
+p <- simpleNetwork(mod1)
+g <- graph.data.frame(d = mod1, directed = FALSE)
 myCols <- setNames(c("#DCE319FF","#DCE319FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF"),
                    c("THAP1", "YY1","ADIPOR2","ADNP","BCKDHA","GALNT2","GPT2",   
                      "ISCU","MAST4","MON1A","PPIF","TBC1D22B",
