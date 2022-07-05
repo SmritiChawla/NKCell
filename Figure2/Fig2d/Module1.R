982ae52e51a998aca0e16e3d495b91251b8d9f61
Smriti Chawla <chawlasmriti_1990@yahoo.co.in>
2022-07-05 11:25:39 +0530

    Update Module1.R

--- Figure2/Fig2d/Module1.R
+++ Figure2/Fig2d/Module1.R
@@ -1,15 +1,17 @@
+##laoding libraries
 library(igraph)
 library(networkD3)
+
+###load Module 1 data
 mod2 = read.table("Module1.csv",sep=",",header=T,stringsAsFactors = F)
+
+##Plotting graph
 p <- simpleNetwork(mod2)
-
 g <- graph.data.frame(d = mod2, directed = FALSE)
-
 myCols <- setNames(c("#DCE319FF","#DCE319FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF","#55C667FF"),
                    c("THAP1", "YY1","ADIPOR2","ADNP","BCKDHA","GALNT2","GPT2",   
                      "ISCU","MAST4","MON1A","PPIF","TBC1D22B",
                      "TTC31","TTLL7","UNC13B","WDR37"))
 V(g)$color <- myCols[V(g)$name]
-
 plot(g, vertex.label = V(g)$name,vertex.label.color="black")
 
