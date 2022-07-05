9b2ade3a900b2d0bc992f0bdbee84fe85441555e
Smriti Chawla <chawlasmriti_1990@yahoo.co.in>
2022-07-05 11:06:26 +0530

    Update Fig1b.py

--- Figure1/Fig1b/ScanpyPlotting/Fig1b.py
+++ Figure1/Fig1b/ScanpyPlotting/Fig1b.py
@@ -1,28 +1,16 @@
+##loading libraries
 import numpy as np
 import pandas as pd
 import scanpy as sc
 
-
- 
+###loading data and metadata
 exp = pd.read_csv("markers_for_scanpy.csv",index_col=0)
 CellTypes = pd.read_csv("groups.csv",index_col=0)
 
+##plotting upregulated genes in Cancer and NK cells
 adata = sc.AnnData(X = exp, obs = CellTypes)
-
-gn_up = ["HMGA1",
-"TACSTD2",
-"ANKRD11"]
-
-
-gn_up = ["KLRD1","LAIR1","TNFRSF9","CCR6"]
-
-
-
+gn_up = ["HMGA1","TACSTD2","ANKRD11"]
 sc.pl.matrixplot(adata, gn_up, groupby="CellTypes",var_group_rotation=0,save=True,cmap='Blues')
 
-
-gn_up = ["HMGA1",
-"TACSTD2",
-"ANKRD11"]
-
+gn_up = ["KLRD1","LAIR1","TNFRSF9","CCR6"]
 sc.pl.matrixplot(adata, gn_up, groupby="CellTypes",var_group_rotation=0,save=True,cmap='Blues')
